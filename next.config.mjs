f21382186f0e259e2d5ceb440b75415119233e4a
Reman <mhznremon79@gmail.com>
2025-10-16 12:56:41 +0545

    sajd

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,26 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
-const withMDX = createMDX(
-);
+const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Ignore the specific cache serialization warnings
+    config.ignoreWarnings = [
+      { module: /fumadocs-mdx\/loader-mdx\.cjs/ },
+      { file: /fumadocs-mdx\/loader-mdx\.cjs/ },
+      { module: /next\/dist\/build\/webpack/ },
+      { file: /next\/dist\/build\/webpack/ }
+    ];
+
+    // Disable file system cache in development
+    if (!isServer) {
+      config.cache = false;
+    }
+
+    return config;
+  },
 };
 
 export default withMDX(config);
